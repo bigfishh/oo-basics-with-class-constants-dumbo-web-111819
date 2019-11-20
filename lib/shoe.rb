baf12b209ec0cf1fc176565e08cea83e2f87118f
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-20 23:11:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  BRANDS.uniq!{|x| x.user_id} = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
